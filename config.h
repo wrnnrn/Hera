f89291b9fe1a8821323083bd8da18857f0711018
wrnnrn <hugogimero@gmail.com>
2026-02-22 19:02:55 +0100

    .

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+// Window
+const int WINDOW_WIDTH = 1600;
+const int WINDOW_HEIGHT = 900;
+const int FPS = 60;
+const Color BACKGROUND_COLOR = Color(77,77,77); 
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
 #include <SFML/Graphics.hpp>
-#include "src/config.h" // Window config file 
-
+#include "config.h"
 using namespace sf;
-String windowName = "Hera - Pantalla Principal";
+
+const char* windowName = "Hera - Pantalla Principal";
+const char* windowTitle = "Secciones";
 
 int main() {
-    // Window Properties
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), windowName);
-    
-    while(window.isOpen()){
-        Event event;
-        while(window.pollEvent(event)){
-            if(event.type==Event::Closed){
-                window.close();
-            }
-        }
 
-        window.clear(Color(77, 77, 77));
+    // Font
+    Font font;
+    if(!font.loadFromFile("assets/Arimo/ArimoNerdFont-Bold.ttf"))
+        return -1;
+
+    // Title Text
+    Text text;
+    text.setFont(font);
+    text.setString(windowTitle);
+    text.setCharacterSize(56);
+
+    // Centrar horizontalmente
+    float windowCenter = window.getSize().x / 2.0f;
+    float textWidth = text.getLocalBounds().width;
+    text.setPosition(windowCenter - textWidth / 2.0f, 20); // 20px desde arriba
+
+    while(window.isOpen()) {
+        Event event;
+        while(window.pollEvent(event))
+            if(event.type == Event::Closed)
+                window.close();
+
+        window.clear(BACKGROUND_COLOR);
+        window.draw(text);
         window.display();
     }
 }
